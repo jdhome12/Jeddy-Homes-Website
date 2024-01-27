5b205b55da96ba77e86fbd7f69bb4cd9f7bb1298
calvinarieri <ariericalvin@gmail.com>
2024-01-27 22:46:18 +0300

    installed:material ui && react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
+      <div>
+        {/* add the website nav bar */}
+        <main>
+
+        </main>
+        {/* add the different pages */}
+        <div>
+          <Routes>
+          </Routes>          
+        </div>
+      </div>
     </>
   );
 }
